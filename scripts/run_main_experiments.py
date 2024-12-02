f6046066f6ff8ad7738beea9eecedae5acbbdce4
zhuokaizhao <zhuokai@uchicago.edu>
2024-12-02 18:51:14 +0000

    Updated run script.

--- scripts/run_main_experiments.py
+++ scripts/run_main_experiments.py
@@ -7,14 +7,14 @@ model_paths = {
     "instructblip": "Salesforce/instructblip-vicuna-7b",
     "llava-next": "llava-hf/llava-v1.6-mistral-7b-hf",
 }
-num_gpus = 2
 all_methods = [
-    # "original",
+    # "greedy",
+    "beam-search",
+    "vcd",
+    # "opera",
     # "uncertainty-guided",
-    "opera",
 ]
 data_dir = "/fsx/zhuokai/maskllama/dataset/COCO"
-all_seeds = list(range(0, 30))
 all_model_names = [
     # "llava-1.5",
     # "instructblip",
@@ -22,8 +22,17 @@ all_model_names = [
 ]
 
 for cur_method in all_methods:
-    for cur_seed in all_seeds:
-        for model_name in all_model_names:
+    for model_name in all_model_names:
+        if model_name == "llava-1.5":
+            all_seeds = [1, 9, 40]
+        elif model_name == "instructblip":
+            all_seeds = [26, 31, 34]
+        elif model_name == "llava-next":
+            all_seeds = [29, 31, 32, 46, 41]
+        else:
+            raise ValueError(f"Unknown model name: {model_name}")
+
+        for cur_seed in all_seeds:
             model_path = model_paths[model_name]
             run_name = f"{cur_method}_{model_name}_seed_{cur_seed}"
             output_dir = f"outputs/{cur_method}_{model_name}"
@@ -32,6 +41,11 @@ for cur_method in all_methods:
             script_dir = os.path.dirname(script_path)
             if not os.path.exists(script_dir):
                 os.makedirs(script_dir, exist_ok=True)
+
+            if cur_method == "opera" and model_name == "llava-next":
+                num_gpus = 2
+            else:
+                num_gpus = 1
 
             with open(script_path, "w") as f:
                 lines_to_write = [
@@ -50,10 +64,14 @@ for cur_method in all_methods:
                     "\n",
                     f"python -m chair_test.chair_test --seed {cur_seed} --method {run_name}_ --coco-data-dir {data_dir} --model-path {model_path} --image-numbers 500 --sample-save-name logs/{run_name}.log --model {model_name} --output-dir {output_dir}",
                 ]
-                if cur_method == "original":
+                if cur_method == "greedy":
                     lines_to_write.append(" --original True\n")
+                elif cur_method == "beam-search":
+                    lines_to_write.append(" --original True --num-beams 3\n")
                 elif cur_method == "opera":
                     lines_to_write.append(" --opera True\n")
+                elif cur_method == "vcd":
+                    lines_to_write.append(" --vcd True\n")
                 else:  # uncertainty-guided
                     lines_to_write.append("\n")
                 for cur_line in lines_to_write:
